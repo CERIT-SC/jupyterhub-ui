1f335c27a0d524decb2783547a88460b8442adaa
Matúš Čík <cik@ics.muni.cz>
2025-09-15 16:28:33 +0200

    dockerfile local env

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,9 +1,5 @@
-import assert from "node:assert";
-
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
-
-assert(process.env.DB_FILE_NAME, "DB_FILE_NAME must be set in .env.local");
 
 const sqlite = new Database(process.env.DB_FILE_NAME);
 
